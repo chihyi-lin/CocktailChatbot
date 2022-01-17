84b7cf0f113585fb3361ad48168c5782b387c270
Kuan-yu <75708899+washiyuyu@users.noreply.github.com>
2022-01-17 02:42:14 +0100

    files management

--- XMLtoDB.py
+++ XMLtoDB.py
@@ -25,6 +25,38 @@ tree = ET.parse('cocktails.xml')
 # in our xml file student is the root for all student data.
 data2 = tree.findall('cocktail')
 
+
+# retrieving the data and insert into table
+# i value for xml data 
+# j value printing number of values that are stored
+for i, j in zip(data2, range(1, 31)):
+    name = i.find('name').text
+    hashtagSet = i.findall('hashtagSet')
+
+    for k, l in zip(hashtagSet, range(1, 6)):
+        hashtag1 = k.find('hashtag1').text
+        
+        try:
+            hashtag2 = k.find('hashtag2').text
+        except:
+            hashtag2 = ""
+
+        try:
+            hashtag3 = k.find('hashtag3').text
+        except:
+            hashtag3 = ""
+
+    # sql query to insert data into database
+    data = """INSERT INTO cocktail(name,hashtag1,hashtag2,hashtag3) VALUES(%s,%s,%s,%s)"""
+  
+    # creating the cursor object
+    c = conn.cursor()
+      
+    # executing cursor object
+    c.execute(data, (name, hashtag1, hashtag2, hashtag3))
+    conn.commit()
+    print("cocktail tabelCocktail No-", j, " stored successfully")
+
 # retrieving the data and insert into table
 # i value for xml data 
 # j value printing number of values that are stored
@@ -96,3 +128,35 @@ for i, j in zip(data2, range(1, 31)):
     conn.commit()
     print("cocktail tableIngredient No-", j, " stored successfully")
 
+
+
+# retrieving the data and insert into table
+# i value for xml data 
+# j value printing number of values that are stored
+for i, j in zip(data2, range(1, 31)):
+    name = i.find('name').text
+    flavor = i.findall('flavor')
+
+    for k, l in zip(flavor, range(1, 6)):
+        taste1 = k.find('taste1').text
+        
+        try:
+            taste2 = k.find('taste2').text
+        except:
+            taste2 = ""
+        
+        try:
+            taste3 = k.find('taste3').text
+        except:
+            taste3 = ""
+
+    # sql query to insert data into database
+    data = """INSERT INTO flavor(name,taste1,taste2,taste3) VALUES(%s,%s,%s,%s)"""
+  
+    # creating the cursor object
+    c = conn.cursor()
+      
+    # executing cursor object
+    c.execute(data, (name, taste1, taste2, taste3))
+    conn.commit()
+    print("cocktail tabelflavor No-", j, " stored successfully")
